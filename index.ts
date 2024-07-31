d7e8fafb92be20c55be65f44777d7b284af9a69a
Ditto sunny <dittosunny95@gmail.com>
2024-07-31 16:31:09 +0530

    mongodb issue solved

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+import { AdminRoute ,VendorRoute } from "./routes";
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/admin',AdminRoute)
+app.use('/vendor',VendorRoute)
+
+
+
+app.listen(8000 , ()=>{
+     console.clear()
+     console.log("server is running on port 8000")
+})
